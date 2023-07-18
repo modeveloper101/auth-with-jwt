39638413eda182d3f0be4986a842faa0b138014a
Mohamed Rashwan <morashwan8@gmail.com>
2023-07-18 17:53:58 +0700

    added refresh token process

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const blogs = [
   },
 ];
 
+let refreshTokens = [];
+
 //a route to get all the stored users
 app.get("/users", (req, res) => {
   res.json(users);
@@ -35,6 +37,20 @@ app.post("/users", async (req, res) => {
     res.status(500).send({ error: "Failed to create a user" });
   }
 });
+//TODO: move to authServer
+//a route to create a new token
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+  jwt.verify(refreshToken, process.env.SECRET_REFRESH_TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ username: user.username });
+    res.json({ accessToken: accessToken });
+  });
+});
+//TODO: move to authServer
 //a route to authenticate user
 app.post("/login", async (req, res) => {
   const user = users.find((user) => user.username === req.body.username);
@@ -44,9 +60,12 @@ app.post("/login", async (req, res) => {
   try {
     if (await bcrypt.compare(req.body.password, user.password)) {
       //using compare method will exclude the salt from the hashed password and compare the two passwords
-      const accessToken = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN);
+      const accessToken = generateAccessToken(user);
+      const refreshToken = jwt.sign(user, process.env.SECRET_REFRESH_TOKEN);
+      refreshTokens.push(refreshToken);
       res.json({
         accessToken: accessToken,
+        refreshToken: refreshToken,
         message: "Logged in successfully!",
       });
     } else {
@@ -56,6 +75,11 @@ app.post("/login", async (req, res) => {
     res.status(500).send({ error: "Failed to login" });
   }
 });
+//TODO: move to authServer
+//a function to generate access token
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, { expiresIn: "20s" });
+}
 //a route to get blogs
 app.get("/blogs", authenticateToken, (req, res) => {
   //calling middleware to authenticate the token before access resources
